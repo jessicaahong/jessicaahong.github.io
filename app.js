c96f5081ff320405cb8969d5ebd665d7550f52f5
jessicaahong <jessiehong9@gmail.com>
2016-04-16 21:16:37 -0700

    adds content to homepage and styles drop down sections

--- app.js
+++ app.js
@@ -1,11 +1,77 @@
 function init() {
 	console.log('javascript linked');
 	loadClickEventListeners();
+	loadAnimationListeners();
 }
 
+function expandModal() {
+	$("#menu-icon-div").removeClass("hidden");
+	$("#transition-div1").removeClass("hidden");
+	$("#transition-div1").addClass("slide-down1");
+	$("#transition-div2").removeClass("hidden");
+	$("#transition-div2").addClass("slide-down2");
+}
+
+function showSection(section) {
+	$(section).removeClass("hidden");
+	$(section).addClass("displayed-modal");
+}
+
+function setHoverAnimation(div, target, animation) {
+	$(div).hover(
+		function(){
+			$(target).addClass(animation);
+		},
+		function(){
+			$(target).removeClass(animation);	
+		}
+	);
+}
 
 function loadClickEventListeners() {
-	$("div.menu-button").on("click", function() {
-		console.log('it was clicked');
+	$("#menu-icon-div").on("click", function() {
+		$(".displayed-modal").addClass("hidden");
+		$("#transition-div1").addClass("hidden");
+		$("#transition-div1").removeClass("slide-down1");
+		$("#transition-div2").addClass("hidden");
+		$("#transition-div2").removeClass("slide-down2");
+		$(".displayed-modal").removeClass("displayed-modal");
+		$("#menu-icon-div").addClass("hidden");
 	});
+	$("#block2").on("click", function() {
+		expandModal();
+		window.setTimeout(function(){
+			showSection("#about-div");
+		}, 500);
+	});
+	$("#block3").on("click", function() {
+		expandModal();
+		window.setTimeout(function(){
+			showSection("#projects-div");
+		}, 500);
+	});
+	$("#block5").on("click", function() {
+		expandModal();
+		window.setTimeout(function(){
+			showSection("#process-div");
+		}, 500);
+	});
+}
+
+function loadAnimationListeners() {
+	setHoverAnimation("#block3", "#pointer-icon", "throb");
+	setHoverAnimation("#gh-icon", "#gh-icon", "grow");
+	setHoverAnimation("#li-icon", "#li-icon", "grow");
+	setHoverAnimation("#mail-icon", "#mail-icon", "grow");
+	setHoverAnimation("#block4", "#emphasis4", "spreading");
+
+	$("#block5").hover(
+		function(){
+			console.log('hovering');
+			$(".lightbulb-div").attr('id','flicker');
+		},
+		function(){
+			$(".lightbulb-div").attr('id','hidden');
+		}
+	);
 }
